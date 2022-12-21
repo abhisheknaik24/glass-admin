42733a0dfd5592c113ec581dcee923a01b5e8500
Abhi24082000 <abhishek.naik@ahwspl.com>
2022-12-21 15:18:45 +0530

    changes

--- app/tasks.py
+++ app/tasks.py
@@ -1,6 +1,11 @@
 from celery import shared_task
 
+from app.models import Cart, Feature, Item, Notification, Production, Sidebar, WorkOrder
+
 
 @shared_task
-def print_statement():
-    return print("Running the process...")
+def work_order_to_production():
+    work_orders = WorkOrder.objects.filter(is_active=True)
+    if work_orders:
+        production = Production()
+    return True
